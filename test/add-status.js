bd65f77788c7add73f3084152f28be36db20b417
Shinichi Kogai <kogai0121@gmail.com>
2015-09-26 15:44:26 +0900

    コンポーネントのテスト検証

--- test/add-status.js
+++ test/add-status.js
@@ -1,14 +1,40 @@
 import assert from 'power-assert';
-import addStatus from 'lib/add-status';
+import _ from 'lodash';
+import jsdom from 'mocha-jsdom';
+import AddStatus from 'lib/add-status';
 
-describe('addStatus', ()=> {
-  it('should have a created hook', ()=> {
-    assert(typeof addStatus.created === 'function');
+describe('AddStatus', ()=> {
+  jsdom();
+
+  it('should extends from Vue class', ()=> {
+    assert(AddStatus.super.name.toString() === 'Vue');
+  });
+
+  it('should have a data initialization function', ()=> {
+    assert(typeof AddStatus.options.data === 'function');
+  });
+
+  it('should have a props array', ()=> {
+    assert(_.isArray(AddStatus.options.props));
+  });
+
+  it('should have a stores as props', ()=> {
+    assert(AddStatus.options.props[0].name === 'stores');
   });
 
   it('should set correct default data', ()=> {
-    const defaultData = addStatus.data();
-    assert(typeof addStatus.data === 'function');
-    assert(defaultData.msg === 'hello!');
+    const addStatusInsatnce = new AddStatus();
+    const initialData = { message: '', emoji: ''};
+    assert(_.isMatch(addStatusInsatnce.newStatus, initialData));
+  });
+
+  it('should can create custom element', ()=> {
+    const AddStatusElement = document.createElement('add-status');
+    AddStatusElement.setAttribute('stores', '{{ stores }}');
+    assert(_.isElement(AddStatusElement));
+  });
+
+  it('should have a onStatusAdd method', ()=> {
+
   });
 });
