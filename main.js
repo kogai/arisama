f1244ab08e5aec9097a1fb640dba82c40c23053a
Shinichi Kogai <kogai0121@gmail.com>
2015-09-29 09:05:04 +0900

    認証モジュールの作成

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+const ipc = require('ipc');
 const menubar = require('menubar');
 const mb = menubar({ preloadWindow: true });
 const templateMenu = require('electron-template-menu');
 
 mb.on('ready', ()=> {
   templateMenu();
+
+  ipc.on('msg-from-renderer', (event, msg)=> {
+    console.log(msg);
+  });
 });
