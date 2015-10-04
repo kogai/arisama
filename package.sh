a42d014ce3cc229f9c6d85848b39202348e7f7bb
Shinichi Kogai <kogai0121@gmail.com>
2015-10-04 16:10:59 +0900

    マイナー/パッチ アップデート用のパッケージスクリプト

--- package.sh
+++ package.sh
@@ -1,10 +1,7 @@
 mkdir build
 cp -r ./lib ./build/lib
-cp index.html ./build/index.html
-cp index.js ./build/index.js
 cp main.js ./build/main.js
 cp package.json ./build/package.json
-cp style.css ./build/style.css
 
 $(npm bin)/electron-packager ./build SlackStatus --platform=darwin --arch=x64 --version=0.33.1 --out ./release/ --overwrite --prune
 zip -9 -r ./release/SlackStatus-darwin-x64.zip ./release/SlackStatus-darwin-x64/
