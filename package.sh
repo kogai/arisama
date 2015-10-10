14a8ee259d78e594f30abbc54bb1cce34fdcb73f
Shinichi Kogai <kogai0121@gmail.com>
2015-10-10 13:56:08 +0900

    [fix] パッケージ前に依存モジュールのインストール

--- package.sh
+++ package.sh
@@ -2,6 +2,9 @@ mkdir build
 cp -r ./lib ./build/lib
 cp main.js ./build/main.js
 cp package.json ./build/package.json
+cd ./build
+NODE_ENV=production npm install
+cd ../
 
 $(npm bin)/electron-packager ./build SlackStatus --platform=darwin --arch=x64 --version=0.33.1 --out ./release/ --overwrite --prune
 zip -9 -r ./release/SlackStatus-darwin-x64.zip ./release/SlackStatus-darwin-x64/
