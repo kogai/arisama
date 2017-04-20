adc8040e5bc161ab7a9500defd224df4e95efe5c
小飼慎一 <kogai0121@gmail.com>
2017-04-20 21:28:12 +0900

    add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate dotenv;
+extern crate iron;
+extern crate mount;
+extern crate router;
+extern crate iron_sessionstorage;
+extern crate urlencoded;
+
+use dotenv::dotenv;
+use std::env;
+use std::io::Read;
+
+use iron::prelude::{Chain, Request, Response};
+use iron::headers::ContentType;
+use iron::{Url, status};
+use mount::Mount;
+use urlencoded::UrlEncodedQuery;
+
 fn main() {
     println!("Hello, world!");
 }
+
