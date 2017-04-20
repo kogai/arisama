797612512e89f95370f4c33428d1aba5cb450aa8
小飼慎一 <kogai0121@gmail.com>
2017-04-20 22:03:37 +0900

    Initial route

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,41 @@ extern crate urlencoded;
 
 use dotenv::dotenv;
 use std::env;
-use std::io::Read;
+// use std::io::Read;
 
-use iron::prelude::{Chain, Request, Response};
+use iron::status;
+use iron::prelude::{Iron, Chain, Request, Response, IronResult};
 use iron::headers::ContentType;
-use iron::{Url, status};
-use mount::Mount;
-use urlencoded::UrlEncodedQuery;
+use router::Router;
+// use mount::Mount;
+// use urlencoded::UrlEncodedQuery;
+// use iron_sessionstorage::traits::*;
+use iron_sessionstorage::SessionStorage;
+use iron_sessionstorage::backends::SignedCookieBackend;
+
+fn get_env(kind: &str) -> String {
+    env::var(kind).expect(&format!("Missing environment parameter {}", kind))
+}
+
+fn handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((ContentType::json().0, status::Ok, "{}")))
+}
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let port = get_env("PORT");
+    let cookie_secret = get_env("COOKIE_SECRET");
+    let mut router = Router::new();
+    router.get("/", handler, "index");
+
+    let mut chain = Chain::new(router);
+    let session = SessionStorage::new(SignedCookieBackend::new(cookie_secret.into_bytes()));
+    chain.link_around(session);
+
+    let server = Iron::new(chain).http(format!("0.0.0.0:{}", port));
+    match server {
+        Ok(listen) => println!("Server starting -> {:?}", listen),
+        Err(err) => println!("Server failed start because {}...", err),
+    }
 }
 
